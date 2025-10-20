028ae0edf0966f2ffd850423c1230aafdb9a7ca9
Yovany Torres <dayorito@gmail.com>
2025-10-19 23:56:48 -0500

    Agregar archivos de proyecto.

---
+++ Calcular.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMulti
+{
+    public class Calcular
+    {
+        public int multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using AppMulti;
+
+Calcular calc = new Calcular();
+
+Console.WriteLine("Ingrese el primer número:");
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Ingrese el segundo número:");
+int num2 = int.Parse(Console.ReadLine() ?? "0");
+int resultado = calc.multiplicar(num1, num2);
+Console.WriteLine($"El resultado de la multiplicación es: {resultado}");
